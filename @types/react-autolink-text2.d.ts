5185f8c33d8c4115529a6652f8537122e801f724
Brennan Moore <brennanmoore@gmail.com>
2021-10-19 18:12:17 -0400

    update favicon

--- @types/react-autolink-text2.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'react-autolink-text2';
--- pages/index.tsx
+++ pages/index.tsx
@@ -120,6 +120,11 @@ export default function Home(props: IProps) {
           href="/rss.xml"
         />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         {/* Open Graph */}
         <meta property="og:url" content="https://www.zamiang.com" key="ogurl" />
         <meta property="og:site_name" content="Kelp" key="ogsitename" />
