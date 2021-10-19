9c5e6d419694871e401413ff71b3f1662ab42705
Brennan Moore <brennanmoore@gmail.com>
2021-10-19 14:11:58 -0400

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 0,
     '@typescript-eslint/restrict-template-expressions': 0,
     '@typescript-eslint/no-unsafe-return': 0,
+    '@typescript-eslint/no-unsafe-argument': 0,
     'arrow-body-style': ['error', 'as-needed'],
     'import/first': 'error',
     'import/no-duplicates': 'error',
