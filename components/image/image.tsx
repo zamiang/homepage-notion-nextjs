64c31dbd31292445e0a8ba57b68aa1fe6c834f80
Brennan Moore <brennanmoore@gmail.com>
2022-01-15 18:21:12 -0500

    doh - ensure nowhere uses s3 urls

--- components/image/image.tsx
+++ components/image/image.tsx
@@ -18,9 +18,9 @@ export const Image = (props: IProps) => {
   const filename = new URL(props.src).pathname.split('/').pop();
   const imageUrl = `https://www.zamiang.com/images/${props.pageId}/${filename}`;
   const src = `${baseURL}/${width}x,q60/${imageUrl}`;
-  const srcSet = `${src} ${props.width}w, ${baseURL}/${width * 2}x,q60/${props.src} ${
+  const srcSet = `${src} ${props.width}w, ${baseURL}/${width * 2}x,q60/${imageUrl} ${
     props.width * 2
-  }w, ${baseURL}/${width * 3}x,q60/${props.src} ${width * 3}w`;
+  }w, ${baseURL}/${width * 3}x,q60/${imageUrl} ${width * 3}w`;
 
   return <img width={width} srcSet={srcSet} alt={props.alt} src={src} loading="lazy" />;
 };
