e767303a79d885f900aeb22461d75014ce18f579
Brennan Moore <brennanmoore@gmail.com>
2025-08-01 21:30:59 -0400

    more images

--- src/components/consts.ts
+++ src/components/consts.ts
@@ -1,3 +1,3 @@
-export const VBC_TITLE = 'Why is Value-based care so difficult?';
+export const VBC_TITLE = 'Why Value-based care is harder than rocket science';
 export const VBC_DESCRIPTION =
-  'This series argues that systemic US healthcare improvements are challenging due to the need to integrate human behavioral changes and business collaboration with scientific advancements. The main thesis is that the current value-based care model, built on insurance claims, is outdated and a new strategy is needed, especially with the rise of AI and consolidated healthcare markets.';
+  'This series argues that systemic US healthcare improvements are challenging due to the need to integrate human behavioral changes and business collaboration with scientific advancements. My main thesis is that the current value-based care model, built on insurance claims, is outdated and a new strategy is needed, especially with the rise of AI and consolidated healthcare markets.';
